d46a9f460f737b57c72b38d2ea043c46dfe35a44
wArturBarros <arturwbarros@gmail.com>
2024-01-03 17:48:43 -0300

    Implementação de propriedades e ajuste de metodos

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,11 +4,18 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        protected string Modelo { get; set; }
+        protected string IMEI { get; set; }
+        protected int Memoria { get; set; }
 
-        public Smartphone(string numero)
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia Teste: ");
+Smartphone nokia = new Nokia(numero: "9876543", modelo: "nokia1100", imei: "095465433194563", memoria: 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("zipzop");
+
+Console.WriteLine("");
+Console.WriteLine("");
+
+Console.WriteLine("Iphone Teste: ");
+Smartphone iphone = new Iphone(numero: "7654321", modelo: "Iphone PlusUltra X", imei: "356303483199848", memoria: 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Spotify");
